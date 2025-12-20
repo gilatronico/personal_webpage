fa53bf52b912869bc3f811140421d16715d64bb7
gilatronico <alejandro97gilabert@gmail.com>
2025-12-20 21:19:55 +0100

    Fix: Simplify handler export to WSGI application directly

--- api/index.py
+++ api/index.py
@@ -11,15 +11,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'landing_project.settings')
 # Import Django WSGI application
 from django.core.wsgi import get_wsgi_application
 
-# Create WSGI application
-django_app = get_wsgi_application()
-
-# For Vercel, we need to export the application
-# The @vercel/python adapter will handle WSGI applications
-def handler(request):
-    """Handler function that Vercel can call"""
-    return django_app
-
-# Also export directly as app (some Vercel configs expect this)
-app = django_app
+# Create WSGI application - this is what Vercel expects
+# The @vercel/python adapter automatically detects WSGI applications
+application = get_wsgi_application()
+handler = application
 
