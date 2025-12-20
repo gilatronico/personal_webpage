2287adf7ab0417709e58c096f54711bcb5ca90de
gilatronico <alejandro97gilabert@gmail.com>
2025-12-20 19:44:54 +0100

    Fix: Simplify WSGI and handle static files in Vercel serverless

--- landing_project/settings.py
+++ landing_project/settings.py
@@ -163,7 +163,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# En Vercel, usar /tmp para staticfiles si es necesario
+if os.environ.get('VERCEL'):
+    STATIC_ROOT = '/tmp/staticfiles'
+else:
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
--- landing_project/wsgi.py
+++ landing_project/wsgi.py
@@ -8,17 +8,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 # Configurar el módulo de settings antes de importar Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'landing_project.settings')
 
-try:
-    from django.core.wsgi import get_wsgi_application
-    application = get_wsgi_application()
-except Exception as e:
-    # En caso de error, loguear para debugging en Vercel
-    import traceback
-    print(f"Error initializing Django WSGI application: {e}")
-    print(traceback.format_exc())
-    sys.exit(1)
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
