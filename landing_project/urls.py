3d3537a1bb39a2adbe297f4627c6c75a83f240da
gilatronico <alejandro97gilabert@gmail.com>
2025-12-20 22:34:38 +0100

    mm

--- landing_project/urls.py
+++ landing_project/urls.py
@@ -16,10 +16,41 @@ urlpatterns = [
 
 # Serve static files - works in both DEBUG and production (Vercel)
 # Use STATICFILES_DIRS to serve from the static directory
+from django.contrib.staticfiles import finders
+from django.http import Http404, HttpResponse
+import os
+
+def serve_static_file(request, path):
+    """Serve static files using Django's staticfiles finder"""
+    # Try to find the file using staticfiles finders
+    static_file = finders.find(path)
+    if static_file:
+        try:
+            with open(static_file, 'rb') as f:
+                content = f.read()
+            # Determine content type
+            if path.endswith('.png'):
+                content_type = 'image/png'
+            elif path.endswith('.jpg') or path.endswith('.jpeg'):
+                content_type = 'image/jpeg'
+            elif path.endswith('.svg'):
+                content_type = 'image/svg+xml'
+            elif path.endswith('.css'):
+                content_type = 'text/css'
+            elif path.endswith('.js'):
+                content_type = 'application/javascript'
+            else:
+                content_type = 'application/octet-stream'
+            
+            response = HttpResponse(content, content_type=content_type)
+            return response
+        except Exception:
+            pass
+    raise Http404("Static file not found")
+
 if settings.STATICFILES_DIRS:
-    static_dir = str(settings.STATICFILES_DIRS[0])
     urlpatterns += [
-        re_path(r'^static/(?P<path>.*)$', static_serve, {'document_root': static_dir}),
+        re_path(r'^static/(?P<path>.*)$', serve_static_file),
     ]
 
 # Serve media files in DEBUG mode
